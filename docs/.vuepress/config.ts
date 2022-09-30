24d7558d318a947e3d9941c186d4d6f69c36ecfa
huangjb <hjbmeow@163.com>
2022-09-30 12:23:41 +0800

    📖 修改前端菜单

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -35,10 +35,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         link: "/web/", //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
           // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
-          {
-            text: "前端文章",
-            items: [{ text: "JavaScript", link: "/pages/8143cc480faf9a11/" }],
-          },
+          // {
+          //   text: "前端文章",
+          //   items: [{ text: "JavaScript", link: "/pages/8143cc480faf9a11/" }],
+          // },
           {
             text: "学习笔记",
             items: [
